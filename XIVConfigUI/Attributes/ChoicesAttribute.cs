84d9c3c3561a14fbb66b80a32e658a69f5c3a41f
秋水 <53346444+ArchiDog1998@users.noreply.github.com>
2024-06-16 20:07:08 +0800

    fix: add if it is lazy.

--- XIVConfigUI/Attributes/ChoicesAttribute.cs
+++ XIVConfigUI/Attributes/ChoicesAttribute.cs
@@ -3,8 +3,9 @@
 [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
 public abstract class ChoicesAttribute : Attribute
 {
+    protected virtual bool Lazy => true;
     private readonly Lazy<Pair[]> _choicesCreator;
-    public Pair[] Choices => _choicesCreator.Value;
+    public Pair[] Choices => Lazy ? _choicesCreator.Value : GetChoices();
     public readonly record struct Pair(string Value, string Show);
 
     protected ChoicesAttribute()
